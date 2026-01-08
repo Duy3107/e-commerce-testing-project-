4bddf8c14aaea95d38e3c6af8f66e78ccb709706
Duy3107 <20521243@gm.uit.edu.vn>
2026-01-08 19:12:57 +0700

    fix

--- 05_Automation_Testing/Selenium_testing/tests/test_temp.py
+++ 05_Automation_Testing/Selenium_testing/tests/test_temp.py
@@ -1,23 +1,23 @@
-import os
-from selenium import webdriver
-
-os.environ["SE_DRIVER_MIRROR_URL"] = "https://msedgedriver.microsoft.com"
-
-def test_open_homepage():
-    driver = webdriver.Edge()  # đơn giản hơn, không cần service
-    driver.get("https://automationexercise.com")
-    assert "Automation" in driver.title
-    driver.quit()
-
-
-
-
+# import os
 # from selenium import webdriver
-# from selenium.webdriver.edge.service import Service
-# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+# os.environ["SE_DRIVER_MIRROR_URL"] = "https://msedgedriver.microsoft.com"
 
 # def test_open_homepage():
-#     driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+#     driver = webdriver.Edge()  # đơn giản hơn, không cần service
 #     driver.get("https://automationexercise.com")
 #     assert "Automation" in driver.title
 #     driver.quit()
+
+
+
+
+# # from selenium import webdriver
+# # from selenium.webdriver.edge.service import Service
+# # from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+# # def test_open_homepage():
+# #     driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+# #     driver.get("https://automationexercise.com")
+# #     assert "Automation" in driver.title
+# #     driver.quit()
